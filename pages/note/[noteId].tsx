2a77ce15486e35fbe9eaa2222beddc70248ca640
jinhyeok15 <ekfrhd97@gmail.com>
2023-04-03 19:13:45 +0900

    Feat: Keyword마다 input 부여하기

--- pages/note/[noteId].tsx
+++ pages/note/[noteId].tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import {useState, useEffect, useCallback, useRef} from "react"
 import { NextPage } from "next"
 import { useRouter } from "next/router"
 import { COR_NUM, ROW_NUM } from "@/constants/note.constant"
@@ -8,35 +8,19 @@ const Note: NextPage = () => {
   const router = useRouter()
   const noteId = router.query.noteId as string
 
-  const [text, setText] = React.useState<string>('')
-  const [keywords, setKeywords] = React.useState<string[]>(
+  const [cursor, setCursor] = useState<number|null>(null)
+
+  const [keywords, setKeywords] = useState<string[]>(
     [...Array(COR_NUM * ROW_NUM)].map(() => '')
   )
 
-  React.useEffect(() => {
-    setText("")
-  }, [keywords])
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
-  }
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const index = keywords.findIndex(str => str === '')
-
+  const handleSubmit = useCallback((index: number, text: string) => {
     if (index >= 0) {
       const cp = [...keywords]
       cp[index] = text
       setKeywords(cp)
     }
-  }
-
-  const removekeyword = (index: number) => {
-    const cp = [...keywords]
-    cp[index] = ''
-    setKeywords(cp)
-  }
+  }, [keywords])
 
   return (
     <div className="flex flex-col justify-center h-screen mb-24">
@@ -44,33 +28,61 @@ const Note: NextPage = () => {
         <h1 className="text-xl font-bold text-lime-500 py-1">Note. {noteId}</h1>
         <div className={`grid grid-cols-4 grid-rows-8 my-4`}>
           {
-            keywords.map((keyword, index: number) => (
+            keywords.map((_, index: number) => (
               <Keyword
-                inputValue={keyword}
                 key={index}
-                onClick={() => { removekeyword(index) }}
+                index={index}
+                onSubmit={handleSubmit}
+                cursor={cursor}
               ></Keyword>
             ))
           }
         </div>
-        <form className="flex flex-row w-full justify-between space-x-2" onSubmit={handleSubmit}>
-          <input className="relative w-full border border-gray-500 rounded-sm outline-none px-1.5" type="text" value={text} onChange={handleChange}/>
-          <button className="px-2 py-1 bg-slate-300 rounded-md" type="submit">
-            Send
-          </button>
-        </form>
       </div>
     </div>
   )
 }
 
-const Keyword = (props: {inputValue: string; onClick: () => void}) => {
+interface KeywordProps {
+  index: number;
+  cursor: number|null;
+  onSubmit: (index: number, text: string) => void;
+}
+
+const Keyword = (props: KeywordProps) => {
+  const {index, cursor} = props;
+  const [text, setText] = useState<string>('')
+  const [focused, setFocused] = useState<Boolean>(index===cursor)
+
+  const inputRef = useRef<null|HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current !== null) {
+      if (focused) {
+        inputRef.current.focus();
+      }
+      else {
+        inputRef.current.blur();
+      }
+    }
+  })
+
   return (
-    <div className={`w-32 h-8 px-1.5 bg-zinc-50 border border-gray-${props.inputValue ? 500 : 300} text-center cursor-pointer`}
-      onClick={props.onClick}
-    >
-      <p className="truncate text-md">{props.inputValue}</p>
-    </div>
+    <input className={`w-32 h-8 px-1.5 bg-zinc-50 border border-gray-${text ? 500 : 300} text-center`}
+      type="text" value={text}
+      onKeyDown={(e: React.KeyboardEvent) => {
+        if (e.key === 'Enter'){
+          props.onSubmit(index, text)
+          setFocused(false)
+        }
+      }}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        const text = e.target.value
+        setText(text)
+      }}
+      ref={inputRef}
+      onClick={() => {setFocused(true)}}
+    ></input>
   )
 }
 
